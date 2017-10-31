61f957ed1e8a2d1e1f8f66f7f44d65fcf33b11b9
Mihir Ochani <mihir.ochani@gmail.com>
2017-10-31 15:06:54 -0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Shell Script to Run Main Class ReadTextFile.java from relative path of working directory src
+java -cp ./InsightData.jar ReadTextFile
+
+#javac -sourcePath ./src ReadTextFile.java
+#java -sourcePath ./src/ ReadTextFile
